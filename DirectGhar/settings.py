f416d43eeae22ab76caaa903ec2f07896a76c5df
vaibhavnagargoje <vaibhavnagargoje381@gmail.com>
2026-01-08 00:11:49 +0530

    tailwind v4 working successfully

--- DirectGhar/settings.py
+++ DirectGhar/settings.py
@@ -173,8 +173,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    ]
 
+STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
