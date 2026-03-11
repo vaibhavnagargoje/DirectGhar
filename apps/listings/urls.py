a7fcf967d3de1b99db7fadb9c55f072254df00a2
Kajal-Gujar <gujarkajal0904@gmail.com>
2026-03-11 15:53:51 +0530

    Modify Directghar on mybranch

--- apps/listings/urls.py
+++ apps/listings/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.listings, name='listings'), 
+    path('', views.listings, name='listings'),
     path('post/', views.create_listing, name='post_property'),
+    path('save-search/', views.save_search, name='save_search'),
+    path('save-search/<int:pk>/delete/', views.delete_saved_search, name='delete_saved_search'),
 ]
